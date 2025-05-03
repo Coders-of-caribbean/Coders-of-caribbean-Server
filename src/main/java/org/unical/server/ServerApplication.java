2ef37c6ed7c9c196ea22552ee37d4d1bf652aae4
garra <garrafa435@gmail.com>
2025-05-03 20:13:00 +0200

    Added solverList bean for testing

--- src/main/java/org/unical/server/ServerApplication.java
+++ src/main/java/org/unical/server/ServerApplication.java
@@ -1,13 +1,22 @@
 package org.unical.server;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.unical.server.config.SolverList;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @SpringBootApplication
 public class ServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        ApplicationContext context = SpringApplication.run(ServerApplication.class, args);
+
+        SolverList solverList = (SolverList) context.getBean("solverList");
+        solverList.getSolvers().forEach(System.out::println);
     }
 
 }
