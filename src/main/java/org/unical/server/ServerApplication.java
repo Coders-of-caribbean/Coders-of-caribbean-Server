6f7d4f63c5c4b449821884a02877dfea501b9352
garra <garrafa435@gmail.com>
2025-05-05 18:23:07 +0200

    Updated ServerApplication. Now the server works with 3 different beans of the same type and non just a bean consisting in a list of Solvable

--- src/main/java/org/unical/server/ServerApplication.java
+++ src/main/java/org/unical/server/ServerApplication.java
@@ -4,19 +4,26 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.unical.server.config.SolverList;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Map;
 
 @SpringBootApplication
+//scansiono i Bean a partire da org.unical.server.solvers: I BEAN DOVRANNO ESSERE QUI DENTRO!
+@ComponentScan(basePackages = "org.unical.server.solvers")
 public class ServerApplication {
 
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(ServerApplication.class, args);
 
-        SolverList solverList = (SolverList) context.getBean("solverList");
-        solverList.getSolvers().forEach(System.out::println);
+        Map<String, Solvable> solvers = context.getBeansOfType(Solvable.class);
+
+        System.out.println("=== Solvers registrati ===");
+        solvers.forEach((name, bean) -> {
+            System.out.println("Bean name: " + name + " | Classe: " + bean.getClass().getName());
+        });
     }
 
 }
