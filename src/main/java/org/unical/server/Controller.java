2bb64d4dd1180d01dc630a7ba715f6ea1cfbc076
garra <garrafa435@gmail.com>
2025-05-07 12:13:27 +0200

    Updated controller. Now it correctly returns data based on Bean name and result

--- src/main/java/org/unical/server/Controller.java
+++ src/main/java/org/unical/server/Controller.java
@@ -1,25 +1,38 @@
 package org.unical.server;
 
 import lombok.AllArgsConstructor;
+import org.apache.naming.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.unical.server.model.Input;
+import org.unical.server.model.Output;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping(value = "/solve", consumes = "application/json")
+@RequestMapping(value = "api/v1/", consumes = "application/json")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @AllArgsConstructor
 public class Controller {
-
     // grazie a @ComponentScan e a @Component, Spring individuerà tutti i bean e li popolerà nella lista.
     //ovviamente, viene definita nel costruttore!
     private List<Solvable> solvers;
 
-    @PostMapping(path="/", produces = "application/json")
-    public ResponseEntity<?> solve(@RequestBody Input input) {
-        solvers.stream().forEach(s -> s.solve(input));
-        //TODO costruire la stringa e restituirla al game.
-        return ResponseEntity.ok().build();
+    //applicationContext è quell'oggetto che memorizza e gestisce tutti i bean.
+    private ApplicationContext applicationContext;
+
+    @PostMapping(path="/solve", produces = "application/json")
+    public ResponseEntity<Output> solve(@RequestBody Input input) {
+        Output output = new Output();
+
+        solvers.forEach(s -> {
+            String result = s.solve(input);
+            String solverName = (String)applicationContext.getBean(s.getClass().getName());
+            output.getResult().put(solverName, result);
+        });
+
+        return ResponseEntity.ok(output);
     }
 }
