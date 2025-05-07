434f81353440999703919094c16bf38bfc3c1551
garra <garrafa435@gmail.com>
2025-05-07 13:02:20 +0200

    Minor changes in Output constructor

--- src/main/java/org/unical/server/model/Output.java
+++ src/main/java/org/unical/server/model/Output.java
@@ -3,11 +3,15 @@ package org.unical.server.model;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
-@NoArgsConstructor
 //EXTENSIBLE!
 public class Output {
     private Map<String, String> result;
+
+    public Output() {
+        result = new HashMap<>();
+    }
 }
