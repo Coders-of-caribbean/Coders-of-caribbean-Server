f87c799d6d738025ef510634cb98af2295949474
angelo <angelo.laface.italia@gmail.com>
2025-05-14 11:22:24 +0200

    changed

--- src/main/java/org/unical/server/solvers/tommardo/sol/Solver.java
+++ src/main/java/org/unical/server/solvers/tommardo/sol/Solver.java
@@ -1,11 +1,11 @@
 package org.unical.server.solvers.tommardo.sol;
 
 import org.springframework.stereotype.Component;
-import org.unical.server.ResponseSolver;
+import org.unical.server.AbstractSolver;
 import org.unical.server.model.Input;
 
 @Component
-public class Solver extends ResponseSolver {
+public class Solver extends AbstractSolver {
 
     @Override
     public String solve(Input input) {
