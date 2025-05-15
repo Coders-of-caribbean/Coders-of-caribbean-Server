77eea5eb2eb12310aa275cb92b3301240aa954bf
garra <garrafa435@gmail.com>
2025-05-15 17:25:44 +0200

    Updated input class with new DTO logic

--- src/main/java/org/unical/server/model/Input.java
+++ src/main/java/org/unical/server/model/Input.java
@@ -5,12 +5,93 @@ import lombok.NoArgsConstructor;
 
 import java.awt.*;
 import java.util.List;
+import java.util.Map;
 
-//Here we define the input recieved from the game
+
+/*
+* STRUCTURE:
+*
+* {
+	thommardo: {
+		x: 12,
+		y: 13,
+		speed: 1,
+		rum: 90,
+		direction: 6,
+		enemies:[
+			tung:{
+				x: 10
+				y: 20
+				speed: <-- posso sapere la velocità delle barche avversarie??
+				rum: <-- posso sapere il rum delle barche avversarie??
+				direction: <-- posso sapere la direction delle barche avversarie??
+			},
+			cucinino:{
+				x: 3
+				y: 7
+				speed: <-- posso sapere la velocità delle barche avversarie??
+				rum: <-- posso sapere il rum delle barche avversarie??
+				direction: <-- posso sapere la direction delle barche avversarie??
+			}],
+		bombs:[{x1,y1,dmg}, {x2,y2,dmg},...],
+		barrels:[{x1,y1,qty}, {x2,y2,qty}]
+	},
+
+	tung: {
+		x: 12,
+		y: 13,
+		speed: 1,
+		rum: 90,
+		direction: 6,
+		enemies:[
+			thommardo:{
+				x: 10
+				y: 20
+				speed: <-- posso sapere la velocità delle barche avversarie??
+				rum: <-- posso sapere il rum delle barche avversarie??
+				direction: <-- posso sapere la direction delle barche avversarie??
+			},
+			cucinino:{
+				x: 3
+				y: 7
+				speed: <-- posso sapere la velocità delle barche avversarie??
+				rum: <-- posso sapere il rum delle barche avversarie??
+				direction: <-- posso sapere la direction delle barche avversarie??
+			}],
+		bombs:[{x1,y1,dmg}, {x2,y2,dmg},...],
+		barrels:[{x1,y1,qty}, {x2,y2,qty}]
+	},
+
+	cucinino: {
+		x: 12,
+		y: 13,
+		speed: 1,
+		rum: 90,
+		direction: 6,
+		enemies:[
+			tung:{
+				x: 10
+				y: 20
+				speed: <-- posso sapere la velocità delle barche avversarie??
+				rum: <-- posso sapere il rum delle barche avversarie??
+				direction: <-- posso sapere la direction delle barche avversarie??
+			},
+			thommardo:{
+				x: 3
+				y: 7
+				speed: <-- posso sapere la velocità delle barche avversarie??
+				rum: <-- posso sapere il rum delle barche avversarie??
+				direction: <-- posso sapere la direction delle barche avversarie??
+			}],
+		bombs:[{x1,y1,dmg}, {x2,y2,dmg},...],
+		barrels:[{x1,y1,qty}, {x2,y2,qty}]
+	},
+
+}
+* */
+
 @Data
 @NoArgsConstructor
 public class Input {
-    private List<Barrel> barrels;
-    private List<Bomb> bombs;
-    private List<Ship> ships;
+    private Map<String, PlayerData> input;
 }
