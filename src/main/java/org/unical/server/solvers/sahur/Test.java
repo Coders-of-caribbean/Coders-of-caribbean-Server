6bbc57372a1eb980aca6a4c7ea05312f385817dd
angelo <angelo.laface.italia@gmail.com>
2025-05-15 20:03:45 +0200

    testing common methods

--- src/main/java/org/unical/server/solvers/sahur/Test.java
+++ src/main/java/org/unical/server/solvers/sahur/Test.java
@@ -1,8 +1,41 @@
 package org.unical.server.solvers.sahur;
 
+import org.unical.server.model.*;
+
+import java.util.List;
+
 public class Test {
     public static void main(String[] args) {
         Solver solver = new Solver();
-        System.out.println(solver.solve(null));
+        PlayerData playerData = new PlayerData();
+
+        Barrel barrel = new Barrel();
+        barrel.x = 0;
+        barrel.y = 0;
+        barrel.setRum(5);
+
+        Mine mine = new Mine();
+        mine.x = 0;
+        mine.y = 0;
+
+        PlayerShip ship = new PlayerShip();
+        ship.x = 0;
+        ship.y = 0;
+        ship.setRum(500);
+        ship.setDirection(0);
+        ship.setSpeed(5);
+
+        EnemyShip enemy = new EnemyShip();
+        enemy.x = 22;
+        enemy.y = 25;
+        enemy.setRum(500);
+        enemy.setDirection(0);
+        enemy.setSpeed(5);
+
+        playerData.setBarrels(List.of(barrel));
+        playerData.setMines(List.of(mine));
+        playerData.setEnemies(List.of(enemy));
+        playerData.setInfo(ship);
+        System.out.println(solver.solve(playerData));
     }
 }
