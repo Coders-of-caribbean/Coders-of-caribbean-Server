31e5b90206cbb78c4295c4a7fec1f56157117427
angelo <angelo.laface.italia@gmail.com>
2025-05-15 19:18:57 +0200

    fix

--- src/main/java/org/unical/server/solvers/example/Solver.java
+++ src/main/java/org/unical/server/solvers/example/Solver.java
@@ -3,12 +3,13 @@ package org.unical.server.solvers.example;
 import org.springframework.stereotype.Component;
 import org.unical.server.AbstractSolver;
 import org.unical.server.model.Input;
+import org.unical.server.model.PlayerData;
 
 
 @Component("terracina panca piana 5*30")
 public class Solver extends AbstractSolver {
     @Override
-    public String solve(Input input) {
+    public String solve(PlayerData input) {
         return "Ninja Fuduli";
     }
 }
--- src/main/java/org/unical/server/solvers/tommardo/sol/Solver.java
+++ src/main/java/org/unical/server/solvers/tommardo/sol/Solver.java
@@ -2,13 +2,13 @@ package org.unical.server.solvers.tommardo.sol;
 
 import org.springframework.stereotype.Component;
 import org.unical.server.AbstractSolver;
-import org.unical.server.model.Input;
+import org.unical.server.model.PlayerData;
 
 @Component("niqqas")
 public class Solver extends AbstractSolver {
 
     @Override
-    public String solve(Input input) {
+    public String solve(PlayerData input) {
         return "borroto puzza!";
     }
 }
