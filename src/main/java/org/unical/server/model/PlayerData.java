d050f4c096f54122bbbec8bbc2dd5bfc4890472a
angelo <angelo.laface.italia@gmail.com>
2025-05-15 20:01:57 +0200

    Aggiunto setter per test, andrebbe rimosso

--- src/main/java/org/unical/server/model/PlayerData.java
+++ src/main/java/org/unical/server/model/PlayerData.java
@@ -1,10 +1,12 @@
 package org.unical.server.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@Setter
 public class PlayerData {
     PlayerShip info;
     private List<Barrel> barrels;
