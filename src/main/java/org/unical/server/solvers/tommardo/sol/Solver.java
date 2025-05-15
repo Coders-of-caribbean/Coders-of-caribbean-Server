5abe654ee8ee03bd2ac22a1b33b64716622d138f
garra <garrafa435@gmail.com>
2025-05-15 15:43:42 +0200

    Solved Issue in AbstractSolver naming convention

--- src/main/java/org/unical/server/solvers/tommardo/sol/Solver.java
+++ src/main/java/org/unical/server/solvers/tommardo/sol/Solver.java
@@ -1,30 +1,22 @@
 package org.unical.server.solvers.tommardo.sol;
 
-import it.unical.mat.embasp.base.Handler;
 import it.unical.mat.embasp.base.InputProgram;
-import it.unical.mat.embasp.base.OptionDescriptor;
-import it.unical.mat.embasp.base.Output;
 import it.unical.mat.embasp.languages.IllegalAnnotationException;
 import it.unical.mat.embasp.languages.ObjectNotValidException;
 import it.unical.mat.embasp.languages.asp.ASPInputProgram;
 import it.unical.mat.embasp.languages.asp.ASPMapper;
 import it.unical.mat.embasp.languages.asp.AnswerSet;
 import it.unical.mat.embasp.languages.asp.AnswerSets;
-import it.unical.mat.embasp.platforms.desktop.DesktopHandler;
-import it.unical.mat.embasp.platforms.desktop.DesktopService;
-import it.unical.mat.embasp.specializations.dlv2.desktop.DLV2DesktopService;
 import org.springframework.stereotype.Component;
-import org.unical.server.ResponseSolver;
+import org.unical.server.AbstractSolver;
 import org.unical.server.model.*;
 import org.unical.server.solvers.tommardo.facts.*;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashSet;
-import java.util.NoSuchElementException;
 import java.util.Set;
 
 @Component("thommardo")
-public class Solver extends ResponseSolver {
+public class Solver extends AbstractSolver {
 
     public Solver(){
         super();
