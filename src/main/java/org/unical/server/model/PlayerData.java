cbfebf7e4aa72c632ca964acfa3fb52c2807393e
garra <garrafa435@gmail.com>
2025-05-15 17:24:16 +0200

    Improved Ship representation logic

---
+++ src/main/java/org/unical/server/model/PlayerData.java
@@ -0,0 +1,10 @@
+package org.unical.server.model;
+
+import java.util.List;
+
+public class PlayerData {
+    PlayerShip info;
+    private List<Barrel> barrels;
+    private List<Mine> mines;
+    private List<EnemyShip> enemies;
+}
