6021a851f93b4a0750ca3560d345643312b2c0e2
garra <garrafa435@gmail.com>
2025-05-15 17:26:31 +0200

    Solvers now work with their own PlayerData

--- src/main/java/org/unical/server/AbstractSolver.java
+++ src/main/java/org/unical/server/AbstractSolver.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.beans.factory.BeanNameAware;
 import org.unical.server.model.Input;
+import org.unical.server.model.PlayerData;
 
 /*
 **** SOSTITUITA ALL'INTERFACCIA SOLVABLE ****
@@ -58,5 +59,5 @@ public abstract class AbstractSolver implements BeanNameAware {
         throw new RuntimeException("Unsupported system: " + system);
     }
 
-    public abstract String solve(Input input);
+    public abstract String solve(PlayerData playerData);
 }
