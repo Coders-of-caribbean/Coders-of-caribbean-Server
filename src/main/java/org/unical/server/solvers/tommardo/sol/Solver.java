3275658bf35bc889d177fcb379b54a5b1c16c52c
garra <garrafa435@gmail.com>
2025-05-16 10:46:33 +0200

    Updated solver to new superClass method

--- src/main/java/org/unical/server/solvers/tommardo/sol/Solver.java
+++ src/main/java/org/unical/server/solvers/tommardo/sol/Solver.java
@@ -25,13 +25,13 @@ public class Solver extends AbstractSolver {
     @Override
     public String solve(PlayerData data) {
         try{
-            //1. class registration
-            registerClasses();
 
-            //2. fixed program (facts)
-            addFacts(data);
+            //1. fixed problem (facts)
+            InputProgram program = new ASPInputProgram();
+            program = addFacts(program, data);
+            handler.addProgram(program);
 
-            //3. variable program (strat)
+            //2. variable program (strategy)
             InputProgram strat = new ASPInputProgram();
             strat.addFilesPath("src/main/java/org/unical/server/solvers/tommardo/encodings/searchForRumStrat");
             handler.addProgram(strat);
@@ -40,10 +40,9 @@ public class Solver extends AbstractSolver {
             //strat.clearAll(); //and add other strategies!
 
             //4. generate answer set (just one since there is just a solution.)
-            AnswerSets answerSets = (AnswerSets) handler.startSync();
-            AnswerSet answerSet = answerSets.getAnswersets().getFirst();
+            AnswerSet result = getAnswerSet();
 
-            for(Object atom: answerSet.getAtoms()){
+            for(Object atom: result.getAtoms()){
                 if(! (atom instanceof SolutionFact))
                     continue;
 
@@ -52,47 +51,12 @@ public class Solver extends AbstractSolver {
 
                 //given the solution, return the current string.
                 System.out.println(beanName + " " + sol.getX() + " "+ sol.getY()+" "+sol.getSpeed());
-                return beanName + " " + sol.getX() + " "+ sol.getY()+" "+sol.getSpeed();
+                return sol.getX() + " "+ sol.getY()+" "+sol.getSpeed();
             }
 
         }catch(Exception e){
             e.printStackTrace();
         }
         return "";
-    }
-
-    public void registerClasses() throws ObjectNotValidException, IllegalAnnotationException {
-        ASPMapper instance = ASPMapper.getInstance();
-
-        instance.registerClass(BarrelFact.class);
-        instance.registerClass(BombFact.class);
-        instance.registerClass(EnemyFact.class);
-        instance.registerClass(PlayerFact.class);
-        instance.registerClass(SolutionFact.class);
-    }
-
-    private void addFacts(PlayerData data) throws Exception {
-        Set<Object> facts = new HashSet<>();
-        InputProgram factsProgram = new ASPInputProgram();
-
-        PlayerShip playerInfo = data.getPlayerInfo();
-        //player data
-        facts.add(new PlayerFact(playerInfo.x, playerInfo.y, playerInfo.getRum()));
-
-        //barrel data
-        for(Barrel b : data.getBarrels())
-            facts.add(new BarrelFact(b.x,b.y));
-
-        //enemies data
-        for(Ship s : data.getEnemiesInfo()){
-            facts.add(new EnemyFact(s.x,s.y,s.getRum()));
-        }
-
-        //mines data
-        for(Mine m : data.getMines())
-            facts.add(new BombFact(m.x,m.y));
-
-        factsProgram.addObjectsInput(facts);
-        handler.addProgram(factsProgram);
     }
 }
