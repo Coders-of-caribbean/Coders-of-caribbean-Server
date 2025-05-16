c8fa645dba057730d86440705a10ce27cd9e84cc
garra <garrafa435@gmail.com>
2025-05-16 11:05:37 +0200

    Removed superfluous local facts

--- src/main/java/org/unical/server/solvers/tommardo/Solver.java
+++ src/main/java/org/unical/server/solvers/tommardo/Solver.java
@@ -1,19 +1,11 @@
-package org.unical.server.solvers.tommardo.sol;
+package org.unical.server.solvers.tommardo;
 
 import it.unical.mat.embasp.base.InputProgram;
-import it.unical.mat.embasp.languages.IllegalAnnotationException;
-import it.unical.mat.embasp.languages.ObjectNotValidException;
 import it.unical.mat.embasp.languages.asp.ASPInputProgram;
-import it.unical.mat.embasp.languages.asp.ASPMapper;
 import it.unical.mat.embasp.languages.asp.AnswerSet;
-import it.unical.mat.embasp.languages.asp.AnswerSets;
 import org.springframework.stereotype.Component;
 import org.unical.server.AbstractSolver;
 import org.unical.server.model.*;
-import org.unical.server.solvers.tommardo.facts.*;
-
-import java.util.HashSet;
-import java.util.Set;
 
 @Component("thommardo")
 public class Solver extends AbstractSolver {
@@ -25,19 +17,18 @@ public class Solver extends AbstractSolver {
     @Override
     public String solve(PlayerData data) {
         try{
-
             //1. fixed problem (facts)
             InputProgram program = new ASPInputProgram();
-            program = addFacts(program, data);
             handler.addProgram(program);
+            addFacts(program, data);
 
             //2. variable program (strategy)
             InputProgram strat = new ASPInputProgram();
             strat.addFilesPath("src/main/java/org/unical/server/solvers/tommardo/encodings/searchForRumStrat");
             handler.addProgram(strat);
 
-            /*NOTE: if we want more implementations:*/
-            //strat.clearAll(); //and add other strategies!
+            /*NOTE if we want more implementations:*/
+            //strat.clearAll(); //and add other strategies into the handler!
 
             //3. generate answer set (just one since there is just a solution.)
             AnswerSet result = getAnswerSet();
