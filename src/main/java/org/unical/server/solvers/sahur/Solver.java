5aa4891266707e584a534344e04d00388240a9e2
nando03lr <nando03lr@gmail.com>
2025-05-21 16:58:13 +0200

    first push

--- src/main/java/org/unical/server/solvers/sahur/Solver.java
+++ src/main/java/org/unical/server/solvers/sahur/Solver.java
@@ -16,6 +16,44 @@ import java.util.logging.Logger;
 
 @Component("sahur")
 public class Solver extends AbstractSolver {
+    private final String encodingsPath = "src/main/java/org/unical/server/solvers/sahur/program/";
 
+    public Solver(){
+        super();
+    }
+
+    @Override
+    public String solve(PlayerData data) {
+        try{
+            handler.removeAll();
+            //1. fixed problem (facts)
+            InputProgram program = new ASPInputProgram();
+            handler.addProgram(program);
+            addFacts(program, data);
+
+            //2. variable program (strategy)
+            addStrategy(data);
+
+            /*NOTE if we want more implementations:*/
+            //strat.clearAll(); //and add other strategies into the handler!
+
+            //3. generate answer set (just one since there is just a solution.)
+            AnswerSet result = getAnswerSet();
+
+            System.out.println(result.toString());
+            //4. get the consequent action
+            return getAction(result);
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return "0 0 0";
+    }
+
+    private void addStrategy(PlayerData data){
+        InputProgram strat = new ASPInputProgram();
+        strat.addFilesPath(encodingsPath + "program");
+        handler.addProgram(strat);
+    }
 }
 
