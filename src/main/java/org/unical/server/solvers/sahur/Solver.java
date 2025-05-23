9bb9679c890eee3853e715a1d8e19bec0b8e5020
angelo <angelo.laface.italia@gmail.com>
2025-05-23 12:39:48 +0200

    stash

--- src/main/java/org/unical/server/solvers/sahur/Solver.java
+++ src/main/java/org/unical/server/solvers/sahur/Solver.java
@@ -1,29 +1,31 @@
 package org.unical.server.solvers.sahur;
 
-
 import it.unical.mat.embasp.base.InputProgram;
 import it.unical.mat.embasp.languages.asp.ASPInputProgram;
 import it.unical.mat.embasp.languages.asp.ASPMapper;
 import it.unical.mat.embasp.languages.asp.AnswerSet;
-import it.unical.mat.embasp.languages.asp.AnswerSets;
 import org.springframework.stereotype.Component;
 import org.unical.server.AbstractSolver;
 
 import org.unical.server.model.PlayerData;
+import org.unical.server.predicates.actions.Move;
 import org.unical.server.solvers.sahur.predicates.TargetFact;
+import org.unical.server.solvers.sahur.program.ASPVariableProgram;
 
-import java.util.Map;
-import java.util.Random;
+import java.util.Optional;
 import java.util.logging.Logger;
 
 @Component("sahur")
 public class Solver extends AbstractSolver {
     private final String encodingsPath = "src/main/java/org/unical/server/solvers/sahur/program/";
 
-    private static ASPInputProgram strategy;
-    private static ASPInputProgram base_program;
-    private static ASPInputProgram path_finding;
-    private static ASPInputProgram nearest_rum;
+    private final ASPInputProgram staticDomain = new ASPInputProgram();
+    private final ASPInputProgram dynamicFacts = new ASPInputProgram();
+    private final ASPInputProgram strategy = new ASPVariableProgram<>(encodingsPath + "modules/target_picker", TargetFact.class);
+    private final ASPInputProgram pathfinding = new ASPVariableProgram<>(encodingsPath + "modules/path_finding", Move.class);
+
+    private final ASPInputProgram path_finding = new ASPInputProgram();
+    private final ASPInputProgram nearest_rum = new ASPInputProgram();
 
     static {
         try {
@@ -36,41 +38,46 @@ public class Solver extends AbstractSolver {
 
     public Solver(){
         super();
-        base_program = new ASPInputProgram();
-        path_finding = new ASPInputProgram();
-        nearest_rum = new ASPInputProgram();
-        nearest_rum.addFilesPath(encodingsPath + "find_rum");
-        path_finding.addFilesPath(encodingsPath + "path_finding");
-        strategy = new ASPInputProgram();
+        fixed.addFilesPath(encodingsPath + "domain");
+        handler.addProgram(fixed);
+        nearest_rum.addFilesPath(encodingsPath + "modules/target_picker");
+        path_finding.addFilesPath(encodingsPath + "modules/path_finding2");
     }
 
-    private TargetFact getTarget(AnswerSet answerSet) {
+    private String nextMove(TargetFact targetFact, PlayerData data) throws Exception {
         try {
-            return (TargetFact) answerSet.getAtoms().stream().filter((predicate) -> predicate instanceof TargetFact).findFirst().get();
-        } catch (Exception e) {
-            return null;
+            addFacts(path_finding, data);
+            path_finding.addObjectInput(targetFact);
+            handler.addProgram(path_finding);
+            AnswerSet result = getAnswerSet();
+            return getAction(result);
+        }finally {
+            path_finding.clearPrograms();
+            handler.removeProgram(path_finding);
         }
     }
+
+    private TargetFact chooseTarget(PlayerData data) throws Exception {
+        try {
+            handler.removeAll();
+            addFacts(nearest_rum, data);
+            handler.addProgram(nearest_rum);
+            AnswerSet result = getAnswerSet();
+            Optional<Object> target = result.getAtoms().stream().filter((predicate) -> predicate instanceof TargetFact).findFirst();
+            return (TargetFact) target.orElseThrow();
+        }finally {
+            nearest_rum.clearPrograms();
+            handler.removeProgram(nearest_rum);
+        }
+    }
+
 
     @Override
     public String solve(PlayerData data) {
         try{
-            handler.removeAll();
-            nearest_rum.clearPrograms();
-            addFacts(nearest_rum, data);
-            handler.addProgram(nearest_rum);
-            AnswerSet result = getAnswerSet();
-            TargetFact target = getTarget(result);
-
-            boolean removed = handler.removeProgram(nearest_rum);
-            handler.removeAll();
-            path_finding.clearPrograms();
-            addFacts(path_finding, data);
-            path_finding.addObjectInput(target);
-            handler.addProgram(path_finding);
-            result = getAnswerSet();
-            handler.removeProgram(path_finding);
-            return getAction(result);
+            current_facts.addObjectInput(chooseTarget(data));
+            TargetFact target = chooseTarget(data);
+            return nextMove(target, data);
 
         }catch(Exception e){
             e.printStackTrace();
